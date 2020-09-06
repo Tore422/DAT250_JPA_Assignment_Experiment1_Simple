12629fadad228d18f2c45526a865c614ace870ca
Tore Berven <tore.berven@student.uib.no>
2020-09-06 17:35:53 +0200

    Finished Simple as part of Experiment 1

---
+++ src/de/vogella/jpa/simple/model/Todo.java
@@ -0,0 +1,18 @@
+package de.vogella.jpa.simple.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String summary;
+    private String description;
+}
